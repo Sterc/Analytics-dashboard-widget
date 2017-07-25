e6e061d087b2aee815bfefb396972a6d628c92dd
Joeke Kloosterman <joeke@sterc.nl>
2017-07-25 14:09:08 +0200

    Update gitignore

--- config.core.php
+++ config.core.php
@@ -1,3 +1,3 @@
 <?php
-define('MODX_CORE_PATH', '/var/www/vhosts/modx2/corDvn78h87wvse/');
+define('MODX_CORE_PATH', '/var/www/vhosts/modx-test/private/core/');
 define('MODX_CONFIG_KEY', 'config');
