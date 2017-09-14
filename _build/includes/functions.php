4e95bc1da7b8c0ee9e76f1861991d0d96b37b026
Roel Zeilstra <roel@sterc.nl>
2017-09-14 11:45:27 +0200

    Removing old version

--- _build/includes/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-function getSnippetContent($filename) {
-    $o = file_get_contents($filename);
-    $o = str_replace('<?php','',$o);
-    $o = str_replace('?>','',$o);
-    $o = trim($o);
-    return $o;
-}
--- config.core.php
+++
@@ -1,3 +0,0 @@
-<?php
-define('MODX_CORE_PATH', '/var/www/vhosts/modx-test/private/core/');
-define('MODX_CONFIG_KEY', 'config');
